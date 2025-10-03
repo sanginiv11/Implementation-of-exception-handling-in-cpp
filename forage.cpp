52375629b3d623ded23912a2320341da3921fc85
Sangini Verma <sanginiv11@gmail.com>
2025-10-03 09:21:29 +0530

    Add files via upload

---
+++ forage.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+int main(){
+    int n1;
+    std:: cout<<"enter age of person : ";
+    std::cin>>n1;
+    try{
+        if(n1<18){
+            throw n1;
+        }
+    
+        else{
+            std::cout <<"Age : "<<n1 << "\n APPROVED "<< "\n" ;
+        }
+    }
+    catch(int a){
+        std::cout << "\nERROR : UNDERAGE !! ( " << a <<")"<< "\n";
+    }
+}
+//OUTPUT
+//enter age of person : 56
+//Age : 56
+// APPROVED
