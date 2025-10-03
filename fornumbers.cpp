81dfcc30447520e471959d70645adaf3092d8d94
Sangini Verma <sanginiv11@gmail.com>
2025-10-03 09:19:44 +0530

    Add files via upload

---
+++ fornumbers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+int main(){
+    float n1,n2,ans;
+    std:: cout<<"enter value of number 1 & 2: ";
+    std::cin>>n1>>n2;
+    try{
+        if(n2==0){
+            throw n2;
+        }
+        else{
+            ans=n1/n2;
+            std::cout<<"answer = " <<ans<<"\n";
+        }
+    }
+    catch(float num){
+        std::cout << "\nERROR : division by " << num << "\n";
+    }
+}
+//OUTPUT
+//enter value of number 1 & 2: 11 24
+//answer = 0.458333
